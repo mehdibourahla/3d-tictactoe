292e7c32994b9c0f1ba48878447fbed64898e860
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-12-01 18:14:17 -0500

    ♻️ Refactor minimax algorithm and evaluateBoard function

--- script.js
+++ script.js
@@ -66,7 +66,6 @@ const TicTacToeGame = (() => {
 
     // Apply the best move
     if (bestMove != null) {
-      // console.log("The best move is: ", bestMove);
       applyMove(bestMove);
     }
   }
@@ -80,7 +79,7 @@ const TicTacToeGame = (() => {
       let maxEval = -Infinity;
       getAvailableMoves().forEach((move) => {
         gameState.levels[move.level][move.index] = "O"; // Assuming AI is "O"
-        let evaluation = minimax(gameState, depth - 1, alpha, beta, false);
+        let evaluation = minimax(gameState, depth - 1, alpha, beta, true);
         gameState.levels[move.level][move.index] = null; // Undo the move
         maxEval = Math.max(maxEval, evaluation);
         alpha = Math.max(alpha, evaluation);
@@ -93,7 +92,7 @@ const TicTacToeGame = (() => {
       let minEval = Infinity;
       getAvailableMoves().forEach((move) => {
         gameState.levels[move.level][move.index] = "X"; // Assuming player is "X"
-        let evaluation = minimax(gameState, depth - 1, alpha, beta, true);
+        let evaluation = minimax(gameState, depth - 1, alpha, beta, false);
         gameState.levels[move.level][move.index] = null; // Undo the move
         minEval = Math.min(minEval, evaluation);
         beta = Math.min(beta, evaluation);
@@ -297,7 +296,8 @@ const TicTacToeGame = (() => {
   }
 
   function resetGame() {
-    window.location.href = "difficultyScreen.html";
+    const difficulty = getDifficulty();
+    window.location.href = `index.html?difficulty=${difficulty}`;
   }
 
   function getAvailableMoves() {
@@ -326,8 +326,107 @@ const TicTacToeGame = (() => {
     } else switchPlayer();
   }
 
-  function evaluateBoard() {
+  function evaluateBoard(gameState) {
+    let score = 0;
+
+    // Iterate through each level and evaluate rows, columns, and diagonals
+    for (let level = 0; level < 4; level++) {
+      for (let i = 0; i < 4; i++) {
+        // Rows and columns
+        score += evaluateLine(
+          gameState.levels[level],
+          [i * 4, i * 4 + 1, i * 4 + 2, i * 4 + 3],
+          "O"
+        );
+        score -= evaluateLine(
+          gameState.levels[level],
+          [i * 4, i * 4 + 1, i * 4 + 2, i * 4 + 3],
+          "X"
+        );
+        score += evaluateLine(
+          gameState.levels[level],
+          [i, i + 4, i + 8, i + 12],
+          "O"
+        );
+        score -= evaluateLine(
+          gameState.levels[level],
+          [i, i + 4, i + 8, i + 12],
+          "X"
+        );
+      }
+
+      // Diagonals within a level
+      score += evaluateLine(gameState.levels[level], [0, 5, 10, 15], "O");
+      score -= evaluateLine(gameState.levels[level], [0, 5, 10, 15], "X");
+      score += evaluateLine(gameState.levels[level], [3, 6, 9, 12], "O");
+      score -= evaluateLine(gameState.levels[level], [3, 6, 9, 12], "X");
+    }
+
+    // Verticals across levels
+    for (let i = 0; i < 16; i++) {
+      let positions = [
+        gameState.levels[0][i],
+        gameState.levels[1][i],
+        gameState.levels[2][i],
+        gameState.levels[3][i],
+      ];
+      score += evaluateLine(positions, [0, 1, 2, 3], "O");
+      score -= evaluateLine(positions, [0, 1, 2, 3], "X");
+    }
+
+    // Cube diagonals
+    score += evaluateCubeDiagonals(gameState, "O");
+    score -= evaluateCubeDiagonals(gameState, "X");
+
+    return score;
+  }
+
+  function evaluateLine(board, positions, player) {
+    let playerCount = 0,
+      emptyCount = 0;
+
+    positions.forEach((pos) => {
+      if (board[pos] === player) {
+        playerCount++;
+      } else if (board[pos] === null) {
+        emptyCount++;
+      }
+    });
+
+    // Adjust the scoring based on playerCount and emptyCount
+    if (playerCount === 4) {
+      return 100; // Maximum score for winning line
+    } else if (playerCount === 3 && emptyCount === 1) {
+      return 10; // Score for potential win
+    } else if (playerCount === 2 && emptyCount === 2) {
+      return 5; // Lower score for less immediate threat
+    }
+
     return 0;
+  }
+
+  function evaluateCubeDiagonals(gameState, player) {
+    // This function evaluates the diagonals that span across levels
+    let score = 0;
+
+    // Define the positions for cube diagonals
+    let cubeDiagonals = [
+      [0, 21, 42, 63], // Diagonal from top left of level 0 to bottom right of level 3
+      [15, 26, 37, 48], // Diagonal from top right of level 0 to bottom left of level 3
+      // Add other cube diagonal positions as needed
+    ];
+
+    cubeDiagonals.forEach((diagonal) => {
+      let line = diagonal.map((index) => {
+        let level = Math.floor(index / 16);
+        let position = index % 16;
+        return gameState.levels[level][position];
+      });
+
+      score += evaluateLine(line, [0, 1, 2, 3], player);
+    });
+
+    return score;
   }
 
   return {
