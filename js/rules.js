e1aef0e24738ed9b080cfbd0bb61d9d42671df7e
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-12-03 22:21:14 -0500

    💄  Remove unnecessary CSS rules and add game rules dialog

--- js/rules.js
+++ js/rules.js
@@ -1,5 +1,9 @@
-const playButton = document.getElementById("playButton");
-
-playButton.addEventListener("click", function (){
-    window.location.href = 'difficultyScreen.html'
+document.getElementById("rulesButton").addEventListener("click", function () {
+  document.getElementById("rulesDialog").classList.remove("hidden");
 });
+
+document
+  .getElementById("closeRulesButton")
+  .addEventListener("click", function () {
+    document.getElementById("rulesDialog").classList.add("hidden");
+  });
